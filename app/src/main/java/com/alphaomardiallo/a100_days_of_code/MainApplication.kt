8d06e8cb51310b29c52e7e6155df6869a0b441f2
Alpha <adiallo@exomind.fr>
2024-01-31 23:53:10 +0100

    Timber added to project

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/MainApplication.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/MainApplication.kt
@@ -2,7 +2,18 @@ package com.alphaomardiallo.a100_days_of_code
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
 
 @HiltAndroidApp
 class MainApplication: Application() {
+
+   override fun onCreate() {
+      super.onCreate()
+
+      if (BuildConfig.DEBUG){
+         plant(Timber.DebugTree())
+      }
+   }
+
 }
