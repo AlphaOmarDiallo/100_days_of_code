0194e049833c9e8ac67ec39b2d50b00a0907c1b6
Alpha <adiallo@exomind.fr>
2024-01-31 23:18:38 +0100

    Setup done in app, test setup left

---
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/MainApplication.kt
@@ -0,0 +1,8 @@
+package com.alphaomardiallo.a100_days_of_code
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application() {
+}
