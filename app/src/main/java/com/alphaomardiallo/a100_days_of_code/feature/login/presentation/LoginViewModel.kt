e5a2db2615f509f9853184e37ec241fce818d228
Alpha <adiallo@exomind.fr>
2024-02-05 22:49:07 +0100

    Sign in with email successful

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginViewModel.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginViewModel.kt
@@ -1,16 +1,22 @@
 package com.alphaomardiallo.a100_days_of_code.feature.login.presentation
 
 import androidx.lifecycle.ViewModel
+import com.alphaomardiallo.a100_days_of_code.feature.login.domain.usecase.CreateUserWithPasswordAndEmail
 import com.alphaomardiallo.a100_days_of_code.feature.login.domain.usecase.SignInUserWithEmailAndPasswordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val signInUserWithEmailAndPasswordUseCase: SignInUserWithEmailAndPasswordUseCase
+    private val signInUserWithEmailAndPasswordUseCase: SignInUserWithEmailAndPasswordUseCase,
+    private val createUserWithPasswordAndEmail: CreateUserWithPasswordAndEmail,
 ): ViewModel() {
 
     fun signInUserWithEmailAndPassword(email: String, password: String){
         signInUserWithEmailAndPasswordUseCase.invoke(email, password)
+    }
+
+    fun createUserWithEmailAndPassword(email: String, password: String){
+        createUserWithPasswordAndEmail.invoke(email, password)
     }
 }
