b4e0d4eaf915013aae7398dbe2ea0d70f9d78450
Alpha <adiallo@exomind.fr>
2024-02-05 23:18:46 +0100

    provider design created

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginScreen.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginScreen.kt
@@ -1,13 +1,21 @@
 package com.alphaomardiallo.a100_days_of_code.feature.login.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,12 +30,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.alphaomardiallo.a100_days_of_code.R
 import com.alphaomardiallo.a100_days_of_code.common.domain.validator.EmailValidator.isValidEmail
 import com.alphaomardiallo.a100_days_of_code.common.domain.validator.PasswordValidator.isValidPassword
@@ -54,15 +64,18 @@ fun LoginScreenContent(
 ) {
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AppLogoAndName()
+
         LoginOrSignInWithEmail(
             loginWithEmail = loginWithEmail,
             registerWithEmail = registerWithEmail
         )
+
+        LoginWithProvider()
     }
 }
 
@@ -71,7 +84,7 @@ private fun AppLogoAndName() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.3f),
+            .fillMaxHeight(0.2f),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -169,5 +182,51 @@ private fun LoginOrSignInWithEmail(
         ) {
             Text(text = stringResource(id = R.string.login_register_button))
         }
+    }
+}
+
+@Composable
+private fun LoginWithProvider() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.login_provider_support_text),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(8.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ProviderBox(link = R.string.login_provider_google_link, description = R.string.login_provider_google_description)
+            ProviderBox(link = R.string.login_provider_github_link, description = R.string.login_provider_github_description)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProviderBox(link: Int = 0, description: Int = 0) {
+    Box(
+        modifier = Modifier
+            .height(60.dp)
+            .width(60.dp)
+            .background(color = Color.Transparent)
+    ) {
+        AsyncImage(
+            model = stringResource(id = link),
+            contentDescription = stringResource(id = description),
+            modifier = Modifier
+                .fillMaxSize()
+                .border(width = 1.dp, color = Color.Black),
+        )
     }
 }
