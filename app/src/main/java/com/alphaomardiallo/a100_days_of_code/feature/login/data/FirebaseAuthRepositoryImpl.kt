30f75646616a5d8b417a6f63cb37cb877a99e52d
Alpha <adiallo@exomind.fr>
2024-02-05 09:17:14 +0100

    Ui creation started

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/data/FirebaseAuthRepositoryImpl.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/data/FirebaseAuthRepositoryImpl.kt
@@ -6,14 +6,47 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
+import timber.log.Timber
 
 class FirebaseAuthRepositoryImpl @Inject constructor() : FirebaseAuthRepository {
 
     private var auth: FirebaseAuth? = null
 
+    private var user: FirebaseUser? = null
+
     override fun initializeFirebaseAuth() {
         auth = Firebase.auth
     }
 
-    override fun getCurrentUser(): FirebaseUser? = auth?.currentUser
+    override fun getCurrentUser(): FirebaseUser? {
+        user = auth?.currentUser
+        return user
+    }
+
+    override fun createUserWithEmailAndPassword(email: String, password: String): Boolean {
+        auth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                user = auth?.currentUser
+                Timber.d("[FirebaseAuth] : create user with email successful")
+            } else {
+                Timber.w("[FirebaseAuth] : create user with email unsuccessful")
+            }
+        }
+
+        return auth?.currentUser != null
+    }
+
+    override fun signInUserWithEmailAndPassword(email: String, password: String): Boolean {
+        auth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                user = auth?.currentUser
+                Timber.d("[FirebaseAuth] : sign in user with email successful")
+
+            } else {
+                Timber.w("[FirebaseAuth] : sign in user with email unsuccessful")
+            }
+        }
+
+        return auth?.currentUser != null
+    }
 }
