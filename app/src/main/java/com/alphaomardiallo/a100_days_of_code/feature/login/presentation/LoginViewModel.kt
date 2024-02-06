f6fd99f7a231f82f79c543828e8e57bd2485d105
Alpha <adiallo@exomind.fr>
2024-02-06 09:27:43 +0100

    Sign in with Github works

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginViewModel.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/login/presentation/LoginViewModel.kt
@@ -1,8 +1,10 @@
 package com.alphaomardiallo.a100_days_of_code.feature.login.presentation
 
+import android.app.Activity
 import androidx.lifecycle.ViewModel
 import com.alphaomardiallo.a100_days_of_code.feature.login.domain.usecase.CreateUserWithPasswordAndEmail
 import com.alphaomardiallo.a100_days_of_code.feature.login.domain.usecase.SignInUserWithEmailAndPasswordUseCase
+import com.alphaomardiallo.a100_days_of_code.feature.login.domain.usecase.SignInWithGithubUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -10,13 +12,18 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val signInUserWithEmailAndPasswordUseCase: SignInUserWithEmailAndPasswordUseCase,
     private val createUserWithPasswordAndEmail: CreateUserWithPasswordAndEmail,
+    private val signInWithGithubUseCase: SignInWithGithubUseCase
 ): ViewModel() {
 
     fun signInUserWithEmailAndPassword(email: String, password: String){
-        signInUserWithEmailAndPasswordUseCase.invoke(email, password)
+        signInUserWithEmailAndPasswordUseCase.invoke(email = email, password = password)
     }
 
     fun createUserWithEmailAndPassword(email: String, password: String){
-        createUserWithPasswordAndEmail.invoke(email, password)
+        createUserWithPasswordAndEmail.invoke(email = email, password = password)
+    }
+
+    fun signInWithGithub(activity: Activity){
+        signInWithGithubUseCase.invoke(activity = activity)
     }
 }
