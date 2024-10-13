8a8236cadfcd0c4526f56fb49f7774f8331679bc
Alpha <adiallo@exomind.fr>
2024-10-13 09:54:07 +0200

    fab added

--- app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/dashboard/presentation/Dashboard.kt
+++ app/src/main/java/com/alphaomardiallo/a100_days_of_code/feature/dashboard/presentation/Dashboard.kt
@@ -2,13 +2,18 @@ package com.alphaomardiallo.a100_days_of_code.feature.dashboard.presentation
 
 import _100_days_of_codeTheme
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,9 +22,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.alphaomardiallo.a100_days_of_code.R
 import com.alphaomardiallo.a100_days_of_code.common.domain.model.Challenge
 import com.alphaomardiallo.a100_days_of_code.common.domain.model.User
 import com.alphaomardiallo.a100_days_of_code.common.presentation.theme.largePadding
@@ -29,6 +37,7 @@ import com.alphaomardiallo.a100_days_of_code.feature.dashboard.presentation.comp
 import com.alphaomardiallo.a100_days_of_code.feature.dashboard.presentation.composable.ProgressSection
 import com.alphaomardiallo.a100_days_of_code.feature.onboarding.presentation.OnBoarding
 import org.koin.androidx.compose.koinViewModel
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,22 +51,37 @@ fun Dashboard(viewModel: DashboardViewModel = koinViewModel()) {
         }
     }
 
-    DashboardContent(
-        user = uiState.value.user,
-        challenges = uiState.value.challenges
-    ) {
-        showDialog = true
-    }
-
-    if (showDialog) {
-        BasicAlertDialog(
-            onDismissRequest = { showDialog = false },
-            modifier = Modifier.fillMaxSize(),
-            properties = DialogProperties(usePlatformDefaultWidth = false)
+    Scaffold(
+        floatingActionButton = {
+            if (uiState.value.user != null) {
+                FloatingActionButton(onClick = { Timber.d("Floating action button clicked") }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "",
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        DashboardContent(
+            paddingValues = paddingValues,
+            user = uiState.value.user,
+            challenges = uiState.value.challenges
         ) {
-            Surface(modifier = Modifier.fillMaxSize()) {
-                OnBoarding {
-                    showDialog = false
+            showDialog = true
+        }
+
+        if (showDialog) {
+            BasicAlertDialog(
+                onDismissRequest = { showDialog = false },
+                modifier = Modifier.fillMaxSize(),
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    OnBoarding {
+                        showDialog = false
+                    }
                 }
             }
         }
@@ -66,12 +90,16 @@ fun Dashboard(viewModel: DashboardViewModel = koinViewModel()) {
 
 @Composable
 private fun DashboardContent(
-    user: User? = null, challenges: List<Challenge?> = emptyList(), showDialog: () -> Unit = {}
+    paddingValues: PaddingValues = PaddingValues(),
+    user: User? = null,
+    challenges: List<Challenge?> = emptyList(),
+    showDialog: () -> Unit = {}
 ) {
     Column(
         Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
+            .padding(paddingValues)
             .padding(largePadding())
     ) {
         // Welcome section
